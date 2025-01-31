df238d32929873504959752d458a3f1d9bc51821
SortSanta <kellypeter767@yahoo.com>
2025-01-31 18:09:54 +0100

    Initial Commit

---
+++ VoltFlow/VoltFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoltFlowApp.swift
+//  VoltFlow
+//
+//  Created by Kelly Randall on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct VoltFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoltFlowTests/VoltFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoltFlowTests.swift
+//  VoltFlowTests
+//
+//  Created by Kelly Randall on 31/01/2025.
+//
+
+import Testing
+@testable import VoltFlow
+
+struct VoltFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
